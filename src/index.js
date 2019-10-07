6dcd46c32d06cf09653d8741a43266608abc2e85
Augusto <gutopssouza@gmail.com>
2019-10-07 16:04:39 -0300

    :construction: Create Sign In empty page :construction: Create Sign Up empty page :construction: Implement routes adding sign in and sign out pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 
-// import { Container } from './styles';
+import Routes from './routes';
 
 export default function Index() {
-  return <View />;
+  return <Routes />;
 }
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+// import { createBottomTabNavigator } from 'react-navigation-tabs';
+// import { createStackNavigator } from 'react-navigation-stack';
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+export default createAppContainer(
+  createSwitchNavigator({
+    SignIn,
+    SignUp,
+  })
+);
